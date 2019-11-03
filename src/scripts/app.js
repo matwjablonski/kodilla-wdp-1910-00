bf1b7ce39466eb7be5386867f1364ccfb76af619
Jagoda Bodnar <jagodabodnar90@gmail.com>
2019-11-03 22:25:30 +0100

    feature/EDP191000-19_Karta-mebla-ocena

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,31 @@
-console.log('hello');
+let allStars = document.querySelectorAll('.stars');
+
+for (let i = 0; i < allStars.length; i++) {
+  allStars[i].addEventListener('mouseover', function (e) {
+    let action = 'add';
+    for (let element of this.children) {
+      element.classList[action]('hover');
+      if (element === e.target) action = 'remove';
+    }
+  });
+}
+
+for (let j = 0; j < allStars.length; j++) {
+  allStars[j].addEventListener('mouseout', function (e) {
+    let action = 'remove';
+    for (let element of this.children) {
+      element.classList[action]('hover');
+      if (element === e.target) action = 'remove';
+    }
+  });
+}
+
+for (let k = 0; k < allStars.length; k++) {
+  allStars[k].addEventListener('click', function (e) {
+    let action = 'add';
+    for (let element of this.children) {
+      element.classList[action]('select', 'full');
+      if (element === e.target) action = 'remove';
+    }
+  });
+}
