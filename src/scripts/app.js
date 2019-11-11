0f51e0995d26d440f6925c122014ec0fc1c16624
Izabela Depa <izabela1depa@gmail.com>
2019-11-11 00:25:35 +0000

    Top seller gallery fix

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -41,6 +41,7 @@ for (let z of allStars) {
   });
 }
 
+// Section Gallery
 const slider = tns({
   container: '.thumbnail-slider-inner',
   mouseDrag: true,
@@ -57,6 +58,8 @@ const galleryPrevArrows = document.querySelectorAll(
   '.gallery-container .gallery-arrow.prev'
 );
 
+const galleryItems = document.querySelectorAll('.gallery-container .gallery-item');
+
 for (let galleryNextArrow of galleryNextArrows) {
   galleryNextArrow.addEventListener('click', function (e) {
     e.preventDefault();
@@ -70,5 +73,32 @@ for (let galleryPrevArrow of galleryPrevArrows) {
     e.preventDefault();
 
     slider.goTo('prev');
+  });
+}
+
+for (let galleryPrevArrow of galleryPrevArrows) {
+  galleryPrevArrow.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    slider.goTo('prev');
+  });
+}
+
+for (let galleryItem of galleryItems) {
+  galleryItem.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    for (let notClickedItem of galleryItems) {
+      notClickedItem.classList.remove('active');
+    }
+
+    let currentGalleryItem = e.currentTarget;
+    currentGalleryItem.classList.add('active');
+    const image = currentGalleryItem.querySelector('img').getAttribute('src');
+
+    let selectedProductImage = currentGalleryItem
+      .closest('.tab-pane')
+      .querySelector('.selected-product img');
+    selectedProductImage.setAttribute('src', image);
   });
 }
