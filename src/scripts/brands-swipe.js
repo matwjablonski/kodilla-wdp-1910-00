9043a3ab73f920e24ae5305e9d8e942eef55355e
Krzysztof Szachna-Krause <kszk@gmx.us>
2019-11-13 13:19:31 +0100

    JS amendment2

--- src/scripts/brands-swipe.js
+++ src/scripts/brands-swipe.js
@@ -1,16 +1,17 @@
 'use strict';
 
 document.querySelector('.carousel-inner').addEventListener('touchstart', startX);
-let clientX, deltaX;
+let clientX;
+let deltaX;
 
-function startX (ev) {
-  clientX = ev.touches[0].clientX;
+function startX (touchEvent) {
+  clientX = touchEvent.touches[0].clientX;
 }
 
 document.querySelector('.carousel-inner').addEventListener('touchend', endX);
 
-function endX (ev) {
-  deltaX = ev.changedTouches[0].clientX - clientX;
+function endX (touchEvent) {
+  deltaX = touchEvent.changedTouches[0].clientX - clientX;
   deltaX > 0 ? pressArrow('left') : pressArrow('right');
 }
 
